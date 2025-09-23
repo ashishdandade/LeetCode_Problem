e0dc4340320e31f0bb2211e8b32b95802074fa23
Ashish Dandade <ashishdandade@gmail.com>
2025-09-23 11:06:42 +0530

    Easy

---
+++ Arrays_Hashing/3110_ScoreOfString.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int scoreOfString(String s) {
+        int sum =0;
+        for(int i=1;i<s.length();i++){
+            int x=(int)s.charAt(i-1);
+            int y= (int)s.charAt(i);
+            sum+=Math.abs(x-y);
+        }
+        return sum;
+    }
+}
